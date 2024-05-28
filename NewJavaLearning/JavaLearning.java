cf7e27b4c2219f90943e63c53a58883bd044499e
BikiLearner <97147323+BikiLearner@users.noreply.github.com>
2024-05-28 18:31:36 +0530

    New commit

--- NewJavaLearning/JavaLearning.java
+++ NewJavaLearning/JavaLearning.java
@@ -2,22 +2,27 @@ package NewJavaLearning;
 
 import NewJavaLearning.tools.*;
 
+import java.io.IOException;
+
 public class JavaLearning  {
-     static public void main(String[] args) throws ClassNotFoundException {
-        int a=234;
-        byte b=(byte) a;
-        int i=0;
-        while(i<=4){
-            System.out.println(i);
-            i++;
-        }
-        int arr[]=new int[45];
-        int r=arr.length;
-        String str;
+     static public void main(String[] args) throws IOException {
+//        int a=234;
+//        byte b=(byte) a;
+//        int i=0;
+//        while(i<=4){
+//            System.out.println(i);
+//            i++;
+//        }
+//        int arr[]=new int[45];
+//        int r=arr.length;
+//        String str;
+//
+//
+//
+//        //good to know
+//        Class.forName("NewJavaLearning.tools.Biki");
 
-
-
-        //good to know
-        Class.forName("NewJavaLearning.tools.Biki");
-    }
+         int num=System.in.read();
+         System.out.println(num);
+     }
 }
