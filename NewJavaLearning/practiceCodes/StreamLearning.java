343422072862e6cbcb0e59ee0f830631a6c6dd29
BikiLearner <97147323+BikiLearner@users.noreply.github.com>
2024-11-06 15:47:43 +0530

    New commit

---
+++ NewJavaLearning/practiceCodes/StreamLearning.java
@@ -0,0 +1,45 @@
+package NewJavaLearning.practiceCodes;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamLearning {
+    public static void main(String [] args){
+
+        //Filter : filter all the elements according to your logic
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        numbers.stream()
+                .filter(n -> n > 5).forEach(n -> System.out.print(n + " "));
+        System.out.println();
+        // Map : It is used to transform elements in a stream by applying a function to
+        // each element and producing a new stream with the results.
+        numbers.stream()
+                .map(n -> n * 24).forEach(n -> System.out.print(n + " "));
+
+        // FlatMap : Given a list of lists of strings,
+        // flatten them into a single list of strings.
+        List<List<String>> listOfLists = Arrays.asList(
+                Arrays.asList("one", "two"),
+                Arrays.asList("three", "four")
+        );
+        listOfLists.stream()
+                .flatMap(Collection::stream)
+                .forEach(n -> System.out.print(n + " "));
+        //Distinct : remove duplicate
+        numbers.stream()
+                .distinct()
+                .forEach(n -> System.out.print(n + " "));
+// result: [1, 2, 3, 4]
+
+
+
+
+
+        // we cannot reuse same stream twice
+        //data.forEach(System.out::println);
+    }
+}
